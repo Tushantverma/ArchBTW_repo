7b8718cb2540f3ceae50238bb66b83255968b800
Your Name <you@example.com>
2022-08-15 21:21:45 +0530

    fixed the db

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -4,10 +4,12 @@ rm alci_repo*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R alci_repo.db.tar.gz *.pkg.tar.zst
+#repo-add -n -R alci_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R archbtw_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
+#archbtw_repo.db
